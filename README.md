2cde8ceb03acc0c26f69fd9bcdb742c7ab00a024
Dylan Howell <dylanleehowell@gmail.com>
2023-11-28 23:05:22 -0600

    Update README

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # thot-site
 
-Hands on my knees selling ads on my thot site
+Example S3 static site setup using AWS CDK
+
+CDK creates the following:
+- S3 bucket with website assets
+- SSL Certificate
+- CloudFront Distribution
+
+Live site can be seen here: https://thot-site.com
+
+...Hands on my knees selling ads on my thot site
---
+++ ui/dist/index.js
@@ -0,0 +1,64 @@
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+var ballRadius = 10;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width - paddleWidth) / 2;
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+var x = canvas.width / 2;
+var y = canvas.height / 2;
+var dx = 2;
+var dy = -2;
+var leftPressed = false;
+var rightPressed = false;
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    drawPaddle();
+    x += dx;
+    y += dy;
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+    if (rightPressed) {
+        paddleX = Math.min(paddleX + 7, canvas.width - paddleWidth);
+    }
+    else if (leftPressed) {
+        paddleX = Math.max(paddleX - 7, 0);
+    }
+}
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = '#0095DD';
+    ctx.fill();
+    ctx.closePath();
+}
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = '#0095DD';
+    ctx.fill();
+    ctx.closePath();
+}
+function keyDownHandler(e) {
+    if (e.key === 'Right' || e.key === 'ArrowRight') {
+        rightPressed = true;
+    }
+    else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+        leftPressed = true;
+    }
+}
+function keyUpHandler(e) {
+    if (e.key === 'Right' || e.key === 'ArrowRight') {
+        rightPressed = false;
+    }
+    else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+        leftPressed = false;
+    }
+}
+setInterval(draw, 10);
